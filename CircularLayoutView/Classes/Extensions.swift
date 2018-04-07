70476ec0fa4af8542272147bb098633fe93e7cb5
Ahmed Khalaf <admin@ahmeds-macbook-pro.local>
2018-04-07 23:23:30 +0200

    Extensions file

---
+++ CircularLayoutView/Classes/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  CircularLayoutView
+//
+//  Created by admin on 4/7/18.
+//
+
+import Foundation
+
+extension CGFloat {
+    var degreesToRadians: CGFloat {
+        get {
+            return (self * .pi) / 180
+        }
+    }
+}
